c566535dc64425b08ee984960bd4f9ab412fb999
kyosuke34 <9mm.vegas@gmail.com>
2024-07-23 05:58:13 +0900

    処理

--- html-css-basic-kadai/kadai_005/bmi.js
+++
@@ -1,9 +0,0 @@
-let height;
-let weight;
-let BMI;
-
-height = 68;
-weight = 1.70;
-BMI = height/(weight * weight);
-
-console.log(BMI);
--- html-css-basic-kadai/memo/bmi.js
+++
@@ -1,8 +0,0 @@
-const a = 1
-const b = 2
-const c = a*b
-const d = a+b
-const e = a-c
-
-// window.alert(c)
-console.log(c)
