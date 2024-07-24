8add9055b16f5007ef3cf4d21b8fab6a161d2ff6
kyosuke34 <9mm.vegas@gmail.com>
2024-07-24 12:51:32 +0900

    訂正

--- javascript-basic-kadai/kadai_007/calc.js
+++ javascript-basic-kadai/kadai_007/calc.js
@@ -4,18 +4,20 @@ let num = Math.floor(Math.random() * 500)
 // コンソールに表示させる
 console.log(num);
 
+// ３と５の倍数の場合
+if(num % 3 ===0 && num % 5 ===0){
+    console.log("３と５の倍数です")
+}
 // ３の倍数の場合
-if (num % 3 === 0){
+else if (num % 3 === 0){
     console.log("3の倍数です");
 }
 // ５の倍数の場合
 else if (num % 5 ===0){
     console.log("５の倍数です");
 }
-// ３と５の倍数の場合
-if(num % 3 ===0 && num % 5 ===0){
-    console.log("３と５の倍数です")
-}
+
+
 // それ以外の場合
 else {
     console.log(num)
