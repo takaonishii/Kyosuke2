c5b4d19a0491e24b6dbc648bbf24e71327b668f2
takaonishii <takaonishii@gmail.com>
2024-11-16 08:58:06 +0900

    店舗とタグの紐付け

--- kita_lunch/app/views.py
+++ kita_lunch/app/views.py
@@ -64,12 +64,12 @@ def store_detail(request, area_id, store_id):
 
     area = Area.objects.get(area_id=area_id)			
     store = Store.objects.get(store_id=store_id)
-    # store_tags = StoreTag.objects.filter(store_id=store_id).all()
-    # for store_tag in store_tags:
-    #     print(store_tag.tag.tag_name)
+    store_tags = StoreTag.objects.filter(store_id=store_id).all()
+    for store_tag in store_tags:
+        print(store_tag.tag.tag_name)
 
-    # context = {"area": area, "store": store, "store_tags": store_tags}
-    context = {"area": area, "store": store}
+    context = {"area": area, "store": store, "store_tags": store_tags}
+    # context = {"area": area, "store": store}
 
     print("store:", store)
 
